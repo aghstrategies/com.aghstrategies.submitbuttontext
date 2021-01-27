be31f08890e9306adfececc993e9ef296c54fd6c
Eli Lisseck <lisseck@gmail.com>
2021-01-27 15:21:30 -0500

    remove console log

--- js/changebuttontext.js
+++ js/changebuttontext.js
@@ -1,7 +1,7 @@
 CRM.$(function ($) {
   // Changes Confirm Contribution Buttons to say Submit
   var buttonText = CRM.vars.submitbuttontext.buttontext;
-console.log(buttonText);
+  
   // get value from php setting and change submit to be that variable
   $('button.crm-form-submit').html(buttonText);
 });
