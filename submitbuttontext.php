1ba312649536ac2055022568f891ec6e98e0a147
Chris Garaffa <chris@aghstrategies.com>
2025-01-03 14:36:17 -0500

    Civix update

--- submitbuttontext.php
+++ submitbuttontext.php
@@ -59,7 +59,6 @@ function submitbuttontext_civicrm_buildForm($formName, &$form) {
   }
 }
 
-
 /**
  * Implements hook_civicrm_postProcess().
  *
@@ -123,39 +122,12 @@ function submitbuttontext_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function submitbuttontext_civicrm_xmlMenu(&$files) {
-  _submitbuttontext_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function submitbuttontext_civicrm_install() {
   _submitbuttontext_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function submitbuttontext_civicrm_postInstall() {
-  _submitbuttontext_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function submitbuttontext_civicrm_uninstall() {
-  _submitbuttontext_civix_civicrm_uninstall();
 }
 
 /**
@@ -167,72 +139,6 @@ function submitbuttontext_civicrm_enable() {
   _submitbuttontext_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function submitbuttontext_civicrm_disable() {
-  _submitbuttontext_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function submitbuttontext_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _submitbuttontext_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function submitbuttontext_civicrm_managed(&$entities) {
-  _submitbuttontext_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function submitbuttontext_civicrm_caseTypes(&$caseTypes) {
-  _submitbuttontext_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function submitbuttontext_civicrm_angularModules(&$angularModules) {
-  _submitbuttontext_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function submitbuttontext_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _submitbuttontext_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -240,9 +146,8 @@ function submitbuttontext_civicrm_alterSettingsFolders(&$metaDataFolders = NULL)
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function submitbuttontext_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
